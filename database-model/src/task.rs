bf112098f9498becd4dcc013ba5424918f158552
zooeywm <zooeywm@gmail.com>
2023-11-16 13:40:44 +0800

    feat(task): Add messgae and resources

--- database-model/src/task.rs
+++ database-model/src/task.rs
@@ -12,6 +12,9 @@ pub struct Model {
     pub body: Json,
     pub r#type: i32,
     pub status: i32,
+    pub message: Option<String>,
+    #[sea_orm(column_type = "JsonBinary", nullable)]
+    pub used_resources: Option<Json>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
